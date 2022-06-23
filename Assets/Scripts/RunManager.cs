8823185c563f8962bc1bb1e1a498c887c9d26115
Jangmanbo <wkddpdnjs99@gmail.com>
2022-06-23 11:34:25 +0900

    add : guage bar
    걸음 수에 따라 게이지바 채우기

--- Assets/Scripts/RunManager.cs
+++ Assets/Scripts/RunManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RunManager : MonoBehaviour
 {
@@ -11,12 +12,14 @@ public class RunManager : MonoBehaviour
     private Animator humanAnimator; // 캐릭터 애니메이터
 
     [SerializeField]
+    private Slider slider;  // 걸음 수에 따른 게이지바
+    [SerializeField]
     private TextMeshProUGUI instructionText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine("Run");
     }
 
     // Update is called once per frame
@@ -37,6 +40,19 @@ public class RunManager : MonoBehaviour
                 break;
             default:
                 break;
+        }
+        if (pedometer.steps < 100)
+            slider.value = pedometer.steps / 100f;
+    }
+    IEnumerator Run()
+    {
+        float fill = 0;
+        while (true)
+        {
+            fill++;
+            slider.value = fill / 100;
+            Debug.Log("slider.value = "+ slider.value);
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
