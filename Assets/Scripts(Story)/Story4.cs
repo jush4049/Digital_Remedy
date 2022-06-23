e9fc750695a775413eeaf6e93d306c5007d449c2
Ju Sang Hyun <96518656+jush4049@users.noreply.github.com>
2022-06-23 21:03:21 +0900

    Add files via upload

---
+++ Assets/Scripts(Story)/Story4.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Story4 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Text NameText; // 등장인물 이름 텍스트
+    public Text ScriptText; // 대사 텍스트
+    public string writerText = "";
+
+    public GameObject ClassImage;
+    public List<KeyCode> skipButton; // 대화를 빠르게 넘길 수 있는 키
+    bool isButtonClicked = false;
+
+    void Start()
+    {
+        StartCoroutine(StoryOn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var element in skipButton) // 버튼 검사
+        {
+            if (Input.GetKeyDown(element))
+            {
+                isButtonClicked = true;
+            }
+        }
+    }
+    public void OnPointerDown(PointerEventData pointerEventData) // 버튼을 누를 시
+    {
+        isButtonClicked = true;
+    }
+    public void OnPointerUp(PointerEventData pointerEventData) // 버튼을 눌렀다가 뗄 시
+    {
+
+    }
+    // 텍스트 타이핑 제거 버전
+    IEnumerator NormalScript(string narrator, string narration)
+    {
+        NameText.text = narrator;
+        writerText = "";
+
+        writerText += narration;
+        ScriptText.text = writerText;
+        yield return null;
+
+        //키를 다시 누를 떄 까지 무한정 대기
+        while (true)
+        {
+            if (isButtonClicked)
+            {
+                isButtonClicked = false;
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator StoryOn()
+    {
+        ClassImage.SetActive(true);
+        yield return StartCoroutine(NormalScript("나레이션", "#4 학교 수업 시간"));
+        yield return StartCoroutine(NormalScript("나레이션", "친구의 도움 덕분에 마음이 안정된 주인공은 열심히 수업을 듣는다."));
+        yield return StartCoroutine(NormalScript("선생님", "자 그러면 어제 못다한 수행평가 발표를 하도록 할게요."));
+        yield return StartCoroutine(NormalScript("주인공", "잘..할수 있을까"));
+        yield return StartCoroutine(NormalScript("나레이션", "주인공은 긴장된 마음으로 학우들 앞에 섰다."));
+        yield return StartCoroutine(NormalScript("선생님", "괜찮겠니? 힘들면 무리해서 하지 않아도 된단다."));
+        yield return StartCoroutine(NormalScript("주인공", "아.. 괜찮아요 선생님 열심히 해볼게요."));
+        yield return StartCoroutine(NormalScript("나레이션", "주인공은 크게 호흡을 내쉰 뒤 천천히 발표를 시작했다."));
+        yield return StartCoroutine(NormalScript("나레이션", "네 번째 게임 '발표 게임'을 시작합니다."));
+        SceneManager.LoadScene("Chapter5");
+    }
+}
---
+++ Assets/Scripts(Story)/Story8_3.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Story8_3 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Text NameText; // 등장인물 이름 텍스트
+    public Text ScriptText; // 대사 텍스트
+    public string writerText = "";
+
+    public List<KeyCode> skipButton; // 대화를 빠르게 넘길 수 있는 키
+    bool isButtonClicked = false;
+
+    void Start()
+    {
+        StartCoroutine(StoryOn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var element in skipButton) // 버튼 검사
+        {
+            if (Input.GetKeyDown(element))
+            {
+                isButtonClicked = true;
+            }
+        }
+    }
+    public void OnPointerDown(PointerEventData pointerEventData) // 버튼을 누를 시
+    {
+        isButtonClicked = true;
+    }
+    public void OnPointerUp(PointerEventData pointerEventData) // 버튼을 눌렀다가 뗄 시
+    {
+
+    }
+    // 텍스트 타이핑 제거 버전
+    IEnumerator NormalScript(string narrator, string narration)
+    {
+        NameText.text = narrator;
+        writerText = "";
+
+        writerText += narration;
+        ScriptText.text = writerText;
+        yield return null;
+
+        //키를 다시 누를 떄 까지 무한정 대기
+        while (true)
+        {
+            if (isButtonClicked)
+            {
+                isButtonClicked = false;
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator StoryOn()
+    {
+        yield return StartCoroutine(NormalScript("상담전화", "네 안녕하세요. 24시간 정신건강상담전화입니다. 무엇을 도와드릴까요?"));
+        yield return StartCoroutine(NormalScript("주인공", "으으.. 심장이 또 두근거려요.. 어떡하죠"));
+        yield return StartCoroutine(NormalScript("상담전화", "자 우선은 진정하시고 숨을 크게 들이마셨다가 내쉬어 보세요."));
+        yield return StartCoroutine(NormalScript("나레이션", "주인공은 상담원의 도움에 따라 진정되었고 무사히 집으로 돌아갔다."));
+        SceneManager.LoadScene("Chapter9");
+    }
+}
